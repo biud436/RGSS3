a27ef88a81f6a954afd84c1c32eed80899d257fd
biud436 <biud436@gmail.com>
2016-07-28 11:59:10 +0900

    .

---
+++ Mouse.rb
@@ -0,0 +1,193 @@
+module Unicode
+  MultiByteToWideChar = Win32API.new('Kernel32','MultiByteToWideChar','llpipi','i')
+  WideCharToMultiByte = Win32API.new('Kernel32','WideCharToMultiByte','llpipipp','i')
+  UTF_8 = 65001
+  
+  def unicode!
+    buf = "\0" * (self.size * 2 + 1)
+    MultiByteToWideChar.call(UTF_8, 0, self, -1, buf, buf.size)
+    buf
+  end
+  
+  def unicode_s
+    buf = "\0" * (self.size * 2 + 1)
+    WideCharToMultiByte.call(UTF_8, 0, self, -1, buf, buf.size, nil, nil)
+    buf.delete("\0")
+  end  
+end
+
+class String
+  include Unicode
+end
+
+module INI
+  WritePrivateProfileStringW = Win32API.new('Kernel32','WritePrivateProfileStringW','pppp','s')
+  GetPrivateProfileStringW = Win32API.new('Kernel32','GetPrivateProfileStringW','ppppip','s')
+  extend self
+  def write_string(app,key,str,file_name)
+    path = ".\\" + file_name 
+    (param = [app,key.to_s,str.to_s,path]).collect! {|i| i.unicode!}
+    success = WritePrivateProfileStringW.call(*param)
+  end
+  def read_string(app_name,key_name,file_name)
+    buf = "\0" * 256
+    path = ".\\" + file_name 
+    (param = [app_name,key_name,path]).collect! {|x| x.unicode!}
+    GetPrivateProfileStringW.call(param[0], param[1],0,buf,256,param[2])
+    buf.unicode_s.unpack('U*').pack('U*')
+  end
+end
+
+class Hash
+  def to_ini(file_name="Default.ini",app_name="Default")
+    self.each { |k, v| INI.write_string(app_name,k.to_s.dup,v.to_s.dup,file_name) }
+  end
+end
+
+module TouchInput
+  
+  FindWindowW = Win32API.new('user32.dll', 'FindWindowW', 'pp', 'l')
+  GetCursorPos = Win32API.new('user32.dll', 'GetCursorPos', 'p', 's')
+  ScreenToClient = Win32API.new('user32.dll', 'ScreenToClient', 'lp', 's')
+  GetAsyncKeyState = Win32API.new('user32.dll', 'GetAsyncKeyState', 'i', 'i')
+  @window_name = INI.read_string('Game', 'Title', 'Game.ini')
+  @handle = FindWindowW.call('RGSS Player'.unicode!, @window_name.unicode!)
+  
+  NONE = 0
+  PRESSED = 1
+  RELEASED = 2
+  PRESS = 3
+  
+  KEY_MAPPER = {
+  :LEFT => 0,
+  :RIGHT => 1,
+  :MIDDLE => 2
+  }
+
+  @old_buffer = Array.new(8, false)
+  @current_buffer = Array.new(8, false)
+  @button_map = []
+  
+  @old_point = {:x => 0, :y => 0}
+  @current_point = {:x => 0, :y => 0}
+  
+  @cr = 0
+  @or = 0
+  
+  @last_key = nil
+  @trigger = Array.new(8, false)
+  
+  @time = Time.now.to_i
+  
+  module_function
+  
+  def update
+    @old_buffer = @current_buffer  
+    @current_buffer.map! {|i| i = false}
+    @button_map.map! {|i| i = NONE}
+    @trigger.map! {|i| i = false }
+    @current_buffer[0] = ((GetAsyncKeyState.call(1) & 0x8000) != 0)
+    @current_buffer[1] = ((GetAsyncKeyState.call(2) & 0x8000) != 0)
+    @current_buffer[2] = ((GetAsyncKeyState.call(4) & 0x8000) != 0)
+    
+    for i in 0...8
+      
+      if @old_buffer[i] == false && @current_buffer[i] == true
+        @button_map[i] = PRESSED
+      elsif @last_key  == PRESS && @current_buffer[i] == false
+        @button_map[i] = @last_key = RELEASED
+        @trigger[i] = true
+      elsif @old_buffer[i] == true && @current_buffer[i] == true
+        @button_map[i] = @last_key = PRESS
+      end
+      
+    end
+    
+    point = [0, 0].pack('l2')
+    GetCursorPos.call(point)
+    ScreenToClient.call(@handle, point)
+    @old_point[:x] = @current_point[:x]
+    @old_point[:y] = @current_point[:y]
+    point = point.unpack('l2')
+    @current_point[:x] = point[0]
+    @current_point[:y] = point[1]
+    
+    if (Time.now.to_i - @time) >= 1
+      
+      @time = Time.now.to_i
+    end
+    
+  end
+  
+  def press?(key)
+    return false if not key.is_a?(Symbol)
+    @button_map[KEY_MAPPER[key]] == PRESS
+  end
+  
+  def up?(key)
+    return false if not key.is_a?(Symbol)
+    @button_map[KEY_MAPPER[key]] == RELEASED
+  end
+  
+  def down?(key)
+    return false if not key.is_a?(Symbol)
+    @trigger[KEY_MAPPER[key]] && @last_key == RELEASED
+  end
+  
+  def x
+    @current_point[:x]
+  end
+  
+  def y
+    @current_point[:y]
+  end
+  
+end
+
+class Window_Selectable < Window_Base
+  def process_cursor_move
+    return unless cursor_movable?
+    last_index = @index
+    cursor_down (Input.trigger?(:DOWN))  if Input.repeat?(:DOWN)
+    cursor_up   (Input.trigger?(:UP))    if Input.repeat?(:UP)
+    cursor_right(Input.trigger?(:RIGHT)) if Input.repeat?(:RIGHT)
+    cursor_left (Input.trigger?(:LEFT))  if Input.repeat?(:LEFT)
+    cursor_pagedown   if !handle?(:pagedown) && Input.trigger?(:R)
+    cursor_pageup     if !handle?(:pageup)   && Input.trigger?(:L)
+    Sound.play_cursor if @index != last_index
+    check_mouse_button if defined? TouchInput
+  end
+  def check_mouse_button
+    mx = TouchInput.x
+    my = TouchInput.y      
+    idx = [[((my - self.y) / (col_max * item_height)), 0].max, item_max - 1].min
+    idx += [[((mx - self.x) / (row_max * item_width)), 0].max, item_max - 1].min
+    self.index = idx
+    if TouchInput.press?(:LEFT)
+      rect = self.item_rect(idx)
+      check_area?(rect) { process_ok if ok_enabled? }
+    end
+    if TouchInput.press?(:RIGHT)
+      check_area?(rect) { process_cancel }
+    end
+  end
+  def check_area?(rect)
+    mx = TouchInput.x
+    my = TouchInput.y 
+    if (mx - self.x) >= rect.x && (mx - self.x) <= rect.x + rect.width &&
+    (my - self.y) >= rect.y && (my - self.y) <= rect.y + rect.height
+      yield
+    end
+  end  
+end
+
+module Input
+  class << self
+    alias xxxx_update update
+  end
+  module_function
+  def update
+    xxxx_update
+    TouchInput.update
+  end
+end
---
+++ XP_TitleManagerEx.rb
@@ -0,0 +1,202 @@
+#==============================================================================
+# ** TitleManagerEx (RPG Maker XP)
+#==============================================================================
+# Name       : TitleManagerEx
+# Author     : 러닝은빛(biud436)
+# Usage      : 
+# EndingManager.ending_setup("엔딩1")
+# EndingManager.ending_setup("엔딩2")
+# EndingManager.ending_setup("엔딩3")
+#==============================================================================
+# ** Tool
+#------------------------------------------------------------------------------
+# 타이틀에서 불러올 그래픽 파일들을 설정합니다
+#==============================================================================
+module Tool
+  
+  # 기본 데이터 로드
+  SYS = load_data("Data/System.rxdata")
+  
+  # 타이틀 커맨드 텍스트 설정
+  MENU = {
+  :NEW => "새로운 게임", 
+  :LOAD => "계속 하기", 
+  :EXIT => "게임 종료"
+  }
+  
+  # 타이틀 이미지 및 BGM 설정
+  RESOURCE = {
+  "기본타이틀" => [SYS.title_name,SYS.title_bgm],
+  "엔딩1" => ["001-Title01","064-Slow07"],
+  "엔딩2" => ["002-Title02","063-Slow06"],
+  "엔딩3" => ["003-Title03","062-Slow05"]
+  }
+  
+end
+ 
+#==============================================================================
+# **  EndingManager
+#------------------------------------------------------------------------------
+# 엔딩값을 temp.dat 파일로 저장합니다
+#==============================================================================
+module EndingManager
+  def self.ending_setup(string)
+    begin
+      File.open("temp.dat","wb") do |file|
+        Marshal.dump(publish_key(string),file)
+      end
+    rescue
+      return false
+    end
+  end
+  def self.load_background
+    begin
+      File.open("temp.dat","rb") do |file|
+        Marshal.load(file)
+      end
+    rescue
+      ending_null
+    end
+  end
+  def self.ending_null
+    ending = {}
+    ending[:version] = 0
+    ending[:username] = "NULL"
+    ending[:n] = Tool::RESOURCE["기본타이틀"]
+    ending
+  end
+  def self.publish_key(string)
+    begin
+      ending = {}
+      ending[:version] = 1000
+      ending[:username] = ENV["USERNAME"]
+      ending[:n] = Tool::RESOURCE[string]
+      ending
+    rescue
+      ending_null    
+    end
+  end
+end
+ 
+#==============================================================================
+# ** Header
+#------------------------------------------------------------------------------
+# 엔딩값을 외부파일에서 불러옵니다
+#==============================================================================
+module Header
+  @@background = nil
+  def self.load
+    f = EndingManager.load_background
+    return f[:version],f[:username],f[:n]
+  end
+  def self.choose_background?
+    if [load[0],load[1]] == [1000,ENV["USERNAME"]]
+      load_background(load[2])
+      return true
+    else
+      @@background = Tool::RESOURCE["기본타이틀"]
+      return false
+    end
+  end 
+  def self.load_background(set)
+    @@background = set
+  end
+  def self.export_background
+    return @@background
+  end
+end
+
+#==============================================================================
+# ** Scene_Title
+#------------------------------------------------------------------------------
+#  이 클래스는 타이틀 화면 처리를 수행합니다.
+#==============================================================================
+class Scene_Title
+  def main
+    pre_title
+    update_title
+    dispose_title
+  end
+  def battle_test
+    if $BTEST
+      battle_test
+      return
+    end    
+  end
+  def load_database
+    $data_actors        = load_data("Data/Actors.rxdata")
+    $data_classes       = load_data("Data/Classes.rxdata")
+    $data_skills        = load_data("Data/Skills.rxdata")
+    $data_items         = load_data("Data/Items.rxdata")
+    $data_weapons       = load_data("Data/Weapons.rxdata")
+    $data_armors        = load_data("Data/Armors.rxdata")
+    $data_enemies       = load_data("Data/Enemies.rxdata")
+    $data_troops        = load_data("Data/Troops.rxdata")
+    $data_states        = load_data("Data/States.rxdata")
+    $data_animations    = load_data("Data/Animations.rxdata")
+    $data_tilesets      = load_data("Data/Tilesets.rxdata")
+    $data_common_events = load_data("Data/CommonEvents.rxdata")
+    $data_system        = load_data("Data/System.rxdata")    
+  end
+  def create_command_window
+    @command_window = Window_Command.new(192, Tool::MENU.values)
+    @command_window.back_opacity = 160
+    @command_window.x = 320 - @command_window.width / 2
+    @command_window.y = 288    
+  end
+  def continue_enabled?
+    @continue_enabled = false
+    for i in 0..3
+      if FileTest.exist?("Save#{i+1}.rxdata")
+        @continue_enabled = true
+      end
+    end
+    if @continue_enabled
+      @command_window.index = 1
+    else
+      @command_window.disable_item(1)
+    end    
+  end
+  def create_background
+    @sprite = Sprite.new
+    if Header.choose_background?
+      @sprite.bitmap = RPG::Cache.title(Header.export_background[0])
+    else 
+      @sprite.bitmap = RPG::Cache.title(Header.export_background[0])
+    end
+  end
+  def play_title_bgm
+    if Header.choose_background?
+      $data_system.title_bgm.name = Header.export_background[1]
+    end
+    $game_system.bgm_play($data_system.title_bgm)    
+  end
+  def pre_title
+    battle_test
+    load_database
+    $game_system = Game_System.new
+    create_background
+    create_command_window
+    continue_enabled?
+    play_title_bgm
+    Audio.me_stop
+    Audio.bgs_stop
+    Graphics.transition    
+  end
+  def update_title
+    loop do
+      Graphics.update
+      Input.update
+      update
+      if $scene != self
+        break
+      end
+    end    
+  end
+  def dispose_title
+    Graphics.freeze
+    @command_window.dispose
+    @sprite.bitmap.dispose
+    @sprite.dispose    
+  end
+end
