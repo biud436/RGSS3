062b87fedb2ca1528c3fb7065f6d575b063c0cb9
biud436 <biud436@gmail.com>
2018-05-27 16:34:37 +0900

    .

--- ActorName20180527.rb
+++
@@ -1,7 +0,0 @@
-class Window_Status < Window_Selectable
-  def draw_actor_name(actor, x, y, width = 112)
-    change_color(hp_color(actor))
-    width = text_size(actor.name).width * 2
-    draw_text(x, y, width, line_height, actor.name)
-  end
-end
