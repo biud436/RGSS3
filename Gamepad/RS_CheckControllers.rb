8453460214ae301631c2b34bd0ed38fce2965dce
biud436 <biud436@gmail.com>
2018-07-12 23:10:22 +0900

    RS.gamepad?

--- Gamepad/RS_CheckControllers.rb
+++ Gamepad/RS_CheckControllers.rb
@@ -5,7 +5,8 @@
 # Author     : 러닝은빛(biud436)
 # Version    : 1.0.0 (2018.07.12)
 # Desc       : 게임 패드를 체크합니다.
-# Functions  : 조건 분기 - 스크립트에서 RS.check_controllers를 체크하십시오.
+# Functions  : 
+# - 조건 분기 - 스크립트에서 RS.check_controllers 또는 RS.gamepad? 를 입력
 #==============================================================================
 $imported = {} if $imported.nil?
 $imported["RS_CheckControllers"] = true
@@ -15,5 +16,8 @@ module RS
     states = "\0" * 4
     RSCheckControllers.call(states)
     states.include?('t')
+  end
+  def self.gamepad?
+    check_controllers
   end
 end
