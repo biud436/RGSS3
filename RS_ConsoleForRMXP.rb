646f466f45576b46eb883c9f79517cf4b7cdfad9
biud436 <biud436@gmail.com>
2018-07-15 15:27:28 +0900

    .

--- RS_ConsoleForRMXP.rb
+++ RS_ConsoleForRMXP.rb
@@ -18,13 +18,13 @@ module Unicode
   WideCharToMultiByte = Win32API.new('Kernel32','WideCharToMultiByte','llpipipp','i')
   UTF_8 = 65001
   def unicode!
-    buf = "\0" * (self.size * 2)
+    buf = "\0" * (self.size * 2 + 1)
     len = MultiByteToWideChar.call(UTF_8, 0, self, -1, 0, 0)
     MultiByteToWideChar.call(UTF_8, 0, self, -1, buf, len)
     buf
   end
   def unicode_s
-    buf = "\0" * (self.size * 2)
+    buf = "\0" * (self.size * 2 + 1)
     WideCharToMultiByte.call(UTF_8, 0, self, -1, buf, buf.size, nil, nil)
     buf.delete("\0")
   end
@@ -34,14 +34,36 @@ class String
 end
 end
 
+module INI
+  WritePrivateProfileStringW = Win32API.new('Kernel32','WritePrivateProfileStringW','pppp','s')
+  GetPrivateProfileStringW = Win32API.new('Kernel32','GetPrivateProfileStringW','ppppip','s')
+  extend self
+  def write_string(app,key,str,file_name)
+    path = ".\\" + file_name
+    (param = [app,key.to_s,str.to_s,path]).collect! {|i| i.unicode!}
+    success = WritePrivateProfileStringW.call(param[0], param[1], param[2], param[3])
+  end
+  def read_string(app_name,key_name,file_name)
+    buf = "\0" * 256
+    path = ".\\" + file_name
+    (param = [app_name,key_name,path]).collect! {|x| x.unicode!}
+    GetPrivateProfileStringW.call(param[0], param[1], 0,buf,256,param[2])
+    buf.unicode_s.unpack('U*').pack('U*')
+  end
+end
+
 module Console
   AllocConsole = Win32API.new('Kernel32', 'AllocConsole', 'v', 's')
   GetStdHandle = Win32API.new('Kernel32', 'GetStdHandle', 'l', 'l')
   WriteConsole = Win32API.new('Kernel32', 'WriteConsole', 'lplpp', 's')
   WriteConsoleW = Win32API.new('Kernel32', 'WriteConsoleW', 'lplpp', 's')
+  SetConsoleTitle = Win32API.new('Kernel32', 'SetConsoleTitleW', 'p', 's')
   
   AllocConsole.call
   @@std_handle = GetStdHandle.call(-11)
+  @@game_title = INI.read_string("Game", "Title", "Game.ini")
+  
+  SetConsoleTitle.call(@@game_title.unicode!)
   
   def self.log(args)
     buf = args.join(" ") + "\r\n"
