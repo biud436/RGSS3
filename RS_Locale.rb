c48b63e8fdf90c37040ecfdda99b20f70acb5655
biud436 <biud436@gmail.com>
2019-05-12 23:51:27 +0900

    init

---
+++ RS_Locale.rb
@@ -0,0 +1,29 @@
+# Author : biud436 
+# Date : 2019.05.12
+# Usage : 
+#   Font.default_name = if Locale.check?('ko-KR')
+#     ["나눔고딕", "VL Gothic"]
+#   else 
+#     ["VL Gothic"]
+#   end
+$imported = {} if $imported.nil?
+$imported["RS_Locale"] = true
+module Locale
+  GetUserDefaultLCID = Win32API.new('Kernel32', 'GetSystemDefaultUILanguage', 'v', 'l')
+  GetUserDefaultLocaleName = Win32API.new('Kernel32', 'GetUserDefaultLocaleName', 'pl', 'l')
+  
+  @@locale = ""
+  c = "\0" * 255
+  GetUserDefaultLocaleName.call(c, 255)
+  @@locale = c.delete("\0")
+  
+  def self.check?(locale)
+    @@locale.include?(locale)
+  end
+  
+  def self.get
+    @@locale
+  end
+  
+end
+
