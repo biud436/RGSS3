0640d9d76b93554b7c3516f97857f90122488235
biud436 <biud436@gmail.com>
2019-12-20 18:05:09 +0900

    RS::ConsoleWarnDetails::ALL constant added

--- RS_ConsoleWarnDetails.rb
+++ RS_ConsoleWarnDetails.rb
@@ -11,71 +11,91 @@ $imported = {} if $imported.nil?
 $imported["RS_ConsoleWarnDetails"] = true
 
 module RS
-    module ConsoleWarnDetails
-        # 오류를 기록할 디버그 로그 파일의 이름
-        # 바탕화면으로 지정하고 싶다면 다음과 같습니다.
-        #   예:) File.join(ENV["HOME"], "Desktop", "debug.log")
-        DEBUG_FILE_NAME = "debug.log"
-
-        # 오류 시간 텍스트
-        TIME_FMT = "오류 시간 : %s"
-
-        # 오류 메시지 텍스트
-        ERROR_MESSAGE_FMT = "메시지 : %s"
-        ERROR_BACKTRACE_FMT = "오류 스택 : "
-        
-        # true이면 콘솔에만 오류를 표시하고 게임을 계속 진행합니다.
-        # false면 오류를 전달하고 오류와 함께 게임이 종료 됩니다.
-        # false면 저장하지 않은 모든 데이터를 잃게 됩니다 (기본값은 false)
-        INLINE = false
-        
-        def self.trace(e)
-        
-          lines = []
-          lines << "----------"
-          lines << sprintf(TIME_FMT, Time.now.to_s)
-          lines << sprintf(ERROR_MESSAGE_FMT, e.message)
-          lines << "----------"
-          lines << ERROR_BACKTRACE_FMT
-          lines << e.backtrace
-          lines << "----------"
-          
-          f = File.open(DEBUG_FILE_NAME, "w+")
-          lines.each { |line| f.puts line }
-          f.close
-          
-          if INLINE
-            puts lines.join("\r\n")
-          else
-            raise lines.join("\r\n")
-          end        
-          
-        end
+  module ConsoleWarnDetails
+    # 오류를 기록할 디버그 로그 파일의 이름
+    # 바탕화면으로 지정하고 싶다면 다음과 같습니다.
+    #   예:) File.join(ENV["HOME"], "Desktop", "debug.log")
+    DEBUG_FILE_NAME = "debug.log"
+    
+    # 오류 시간 텍스트
+    TIME_FMT = "오류 시간 : %s"
+    
+    # 오류 메시지 텍스트
+    ERROR_MESSAGE_FMT = "메시지 : %s"
+    ERROR_BACKTRACE_FMT = "오류 스택 : "
+    
+    # true이면 콘솔에만 오류를 표시하고 게임을 계속 진행합니다.
+    # false면 오류를 전달하고 오류와 함께 게임이 종료 됩니다.
+    # false면 저장하지 않은 모든 데이터를 잃게 됩니다 (기본값은 false)
+    INLINE = false
+    
+    # true이면 모든 영역에서 자세한 오류 출력
+    # false면 Game_Character와 Game_Interpreter에서만 자세한 오류 출력
+    # true을 사용하면 자세한 오류는 출력하지만 
+    # 오류가 난 스크립트 에디터 라인을 자동으로 띄워주는 기능은 사용할 수 없게 됩니다.
+    ALL = false
+    
+    def self.trace(e)
+      
+      lines = []
+      lines << "----------"
+      lines << sprintf(TIME_FMT, Time.now.to_s)
+      lines << sprintf(ERROR_MESSAGE_FMT, e.message)
+      lines << "----------"
+      lines << ERROR_BACKTRACE_FMT
+      lines << e.backtrace
+      lines << "----------"
+      
+      f = File.open(DEBUG_FILE_NAME, "w+")
+      lines.each { |line| f.puts line }
+      f.close
+      
+      if INLINE
+        puts lines.join("\r\n")
+      else
+        raise lines.join("\r\n")
+      end        
+      
     end
+  end
 end
 
-class Game_Character < Game_CharacterBase
-  alias detail_error_update_routine_move update_routine_move
-  def update_routine_move
+if RS::ConsoleWarnDetails::ALL
+  
+  alias detail_error_rgss_main rgss_main
+  def rgss_main(&block)
     begin
-      detail_error_update_routine_move
+      detail_error_rgss_main(&block)
     rescue => e
       RS::ConsoleWarnDetails.trace(e)
     end
-  end  
-end
-
-class Game_Interpreter
-  def command_355
-    script = @list[@index].parameters[0] + "\n"
-    while next_event_code == 655
-      @index += 1
-      script += @list[@index].parameters[0] + "\n"
-    end
-    begin
-      eval(script)
-    rescue => e
-      RS::ConsoleWarnDetails.trace(e)
-    end
-  end  
+  end
+  
+else
+  
+  class Game_Character < Game_CharacterBase
+    alias detail_error_update_routine_move update_routine_move
+    def update_routine_move
+      begin
+        detail_error_update_routine_move
+      rescue => e
+        RS::ConsoleWarnDetails.trace(e)
+      end
+    end  
+  end
+  
+  class Game_Interpreter
+    def command_355
+      script = @list[@index].parameters[0] + "\n"
+      while next_event_code == 655
+        @index += 1
+        script += @list[@index].parameters[0] + "\n"
+      end
+      begin
+        eval(script)
+      rescue => e
+        RS::ConsoleWarnDetails.trace(e)
+      end
+    end  
+  end
 end
