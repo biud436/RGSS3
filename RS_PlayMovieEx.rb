42fcf60b1e3ad33e4e6f7bc90d16644a333fcb08
biud436 <biud436@gmail.com>
2020-03-24 18:02:28 +0900

    화면 녹화 기능 추가

--- RS_PlayMovieEx.rb
+++ RS_PlayMovieEx.rb
@@ -17,6 +17,50 @@
 #
 $imported = {} if $imported.nil?
 $imported["RS_PlayMovieEx"] = true
+
+if not defined?(Unicode)
+  module Unicode
+    MultiByteToWideChar = Win32API.new('Kernel32','MultiByteToWideChar','llpipi','i')
+    WideCharToMultiByte = Win32API.new('Kernel32','WideCharToMultiByte','llpipipp','i')
+    UTF_8 = 65001
+    def unicode!
+      buf = "\0" * (self.size * 2 + 1)
+      MultiByteToWideChar.call(UTF_8, 0, self, -1, buf, buf.size)
+      buf
+    end
+    def unicode_s
+      buf = "\0" * (self.size * 2 + 1)
+      WideCharToMultiByte.call(UTF_8, 0, self, -1, buf, buf.size, nil, nil)
+      buf.delete("\0")
+    end
+  end
+  class String
+    include Unicode
+  end
+  module INI
+    WritePrivateProfileStringW = Win32API.new('Kernel32','WritePrivateProfileStringW','pppp','s')
+    GetPrivateProfileStringW = Win32API.new('Kernel32','GetPrivateProfileStringW','ppppip','s')
+    extend self
+    def write_string(app,key,str,file_name)
+      path = ".\\" + file_name
+      (param = [app,key.to_s,str.to_s,path]).collect! {|i| i.unicode!}
+      success = WritePrivateProfileStringW.call(*param)
+    end
+    def read_string(app_name,key_name,file_name)
+      buf = "\0" * 256
+      path = ".\\" + file_name
+      (param = [app_name,key_name,path]).collect! {|x| x.unicode!}
+      GetPrivateProfileStringW.call(param[0], param[1],0,buf,256,param[2])
+      buf.unicode_s.unpack('U*').pack('U*')
+    end
+  end
+  class Hash
+    def to_ini(file_name="Default.ini",app_name="Default")
+      self.each { |k, v| INI.write_string(app_name,k.to_s.dup,v.to_s.dup,file_name) }
+    end
+  end
+end
+
 module FFMPEG
   
   HWND = `powershell (Get-Process -Name "Game").MainWindowHandle`.to_i
@@ -119,12 +163,14 @@ module FFMPEG
 
   end
   
+  # 화면 녹화
   def screen_record(filename, time=10)
-    title = `powershell (Get-Process -Name "Game").MainWindowTitle`
+    File.delete(filename) if FileTest.exist?(filename)
     Thread.new do 
-      `ffmpeg -f gdigrab -framerate 30 -t #{time} -i desktop -vcodec libx264 Movies/#{filename}.mkv`
+      title_name = INI.read_string('Game', 'Title', 'Game.ini')
+      `ffmpeg -f gdigrab -framerate 30 -t #{time} -i title=#{title_name} Movies/#{filename}.mkv`
     end
-  end
+  end  
 end
 
 module Graphics
