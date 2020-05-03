9e692fe847eb9a4d18e323158dade0dc7c3aa363
biud436 <biud436@gmail.com>
2020-05-03 16:36:08 +0900

    .

--- RS_LayeredGameOverImage.rb
+++ RS_LayeredGameOverImage.rb
@@ -1,14 +1,10 @@
 # Author : biud436
-
-$imported = {} if $imported.nil?
-$imported["RS_LayeredGameOverImage"] = true
-
 module SceneManager
   def self.snapshot_for_background
     @background_bitmap.dispose if @background_bitmap
     @background_bitmap = Graphics.snap_to_bitmap
-    @background_bitmap.blur if @stack.size > 0 && !@stack[-1].is_a?(Scene_GameOver)
-  end  
+    @background_bitmap.blur
+  end
 end
 class Sprite_GameOver < Sprite
   def initialize
